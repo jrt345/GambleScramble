0a9dd57955a6417a904ee1a95223748d83199c36
Jrt345 <JonTech543@protonmail.com>
2021-11-27 21:47:52 -0800

    Create a base for gambleScramble.fxml

--- src/main/java/game/Controller.java
+++ src/main/java/game/Controller.java
@@ -1,14 +1,5 @@
 package game;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to GambleScramble!");
-    }
 }
--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gambleScramble.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 550);
         stage.setTitle("GambleScramble");
         stage.setScene(scene);
         stage.show();
