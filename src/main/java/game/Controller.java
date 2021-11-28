8e4d80d09dc8f36d4d7c396e07ed399125ba4a2c
Jrt345 <JonTech543@protonmail.com>
2021-11-28 01:09:01 -0800

    Add on-click button methods to Controller.java and gambleScramble.fxml.

--- src/main/java/game/Controller.java
+++ src/main/java/game/Controller.java
@@ -1,5 +1,32 @@
 package game;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
 
+    @FXML
+    private Button coinTossButton;
+
+    @FXML
+    private Button diceRollButton;
+
+    @FXML
+    private Button handGuessButton;
+
+    @FXML
+    void playCoinToss(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playDiceRoll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playHandGuess(ActionEvent event) {
+
+    }
 }
