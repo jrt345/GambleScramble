ed6fe87627c70308c668c0949b732d86726bc7ff
Jrt345 <JonTech543@protonmail.com>
2021-11-30 20:02:38 -0800

    Create getters and setters for Player.java

--- src/main/java/game/utils/Player.java
+++ src/main/java/game/utils/Player.java
@@ -5,4 +5,36 @@ public class Player {
     int streak;
     Theme theme;
     Currency currency;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getStreak() {
+        return streak;
+    }
+
+    public void setStreak(int streak) {
+        this.streak = streak;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
 }
