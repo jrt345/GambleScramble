1a500f6faddfd5da3657858256b81755c2d2e717
Jrt345 <JonTech543@protonmail.com>
2021-11-30 19:55:59 -0800

    Add Theme.java enum class

---
+++ src/main/java/game/utils/Player.java
@@ -0,0 +1,7 @@
+package game.utils;
+
+public class Player {
+    int balance;
+    int streak;
+    Theme theme;
+}
---
+++ src/main/java/game/utils/Theme.java
@@ -0,0 +1,5 @@
+package game.utils;
+
+public enum Theme {
+    LIGHT, DARK, LIGHT_MINIMAL, DARK_MINIMAL
+}
