94c0bda6bba21a03e0b6ea41d30ef32accc3ad29
Jrt345 <JonTech543@protonmail.com>
2021-12-05 19:11:12 -0800

    Create gameTemplate.fxml design and controller data to GameController.java

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -1,5 +1,37 @@
 package game;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class GameController {
 
+    @FXML
+    private Label gameTitle;
+
+    @FXML
+    private Label gameDetails;
+
+    @FXML
+    private Label gameChoicesLabel;
+
+    @FXML
+    private ChoiceBox<?> gameChoices;
+
+    @FXML
+    private TextField betInput;
+
+    @FXML
+    private Label betInputWarning;
+
+    @FXML
+    private Button placeBetButton;
+
+    @FXML
+    void placeBet(ActionEvent event) {
+
+    }
 }
