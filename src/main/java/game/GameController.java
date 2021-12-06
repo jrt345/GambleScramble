caf0452a16779e8ae41f6697a74a391ae3d67998
Jrt345 <JonTech543@protonmail.com>
2021-12-05 19:36:16 -0800

    Add imageViews from gameTemplate.fxml to GameController.java

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -6,11 +6,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class GameController {
 
     @FXML
     private Label gameTitle;
+
+    @FXML
+    private ImageView gameImage1;
+
+    @FXML
+    private ImageView gameImage2;
 
     @FXML
     private Label gameDetails;
