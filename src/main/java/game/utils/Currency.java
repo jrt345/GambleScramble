7266f95d78b51b877fca86a775c05228f3af7aae
Jrt345 <JonTech543@protonmail.com>
2021-12-12 10:09:41 -0800

    Add "Symbol" string to Currency.java enum class

--- src/main/java/game/utils/Currency.java
+++ src/main/java/game/utils/Currency.java
@@ -1,5 +1,20 @@
 package game.utils;
 
 public enum Currency {
-    USD, EUR, GBP, AUD, CAD, NZD,
+    USD("$"),
+    EUR("€"),
+    GBP("£"),
+    AUD("A$"),
+    CAD("C$"),
+    NZD("NZ$");
+
+    private final String symbol;
+
+    Currency(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
