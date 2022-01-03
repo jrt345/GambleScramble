1d7dd71f291c4e7e8fabc7090ef19dde183b3a83
Jrt345 <JonTech543@protonmail.com>
2022-01-03 07:21:54 -0800

    Made Controller.java open options.fxml in "Options" button

--- src/main/java/game/Controller.java
+++ src/main/java/game/Controller.java
@@ -8,9 +8,14 @@ import game.utils.Player;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -56,8 +61,18 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    void openOptions(ActionEvent event) {
+    void openOptions(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("options.fxml"));
+        Parent root = fxmlLoader.load();
 
+        OptionsController controller = fxmlLoader.getController();
+
+        Stage stage = new Stage();
+        stage.setTitle("Options");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(new Scene(root, 500, 300));
+        stage.setResizable(false);
+        stage.show();
     }
 
     @FXML
