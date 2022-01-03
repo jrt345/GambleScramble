86ed3c8d8b54d0dcd1968ac697aefa5e3d578a33
Jrt345 <JonTech543@protonmail.com>
2022-01-03 07:29:46 -0800

    Connect fxml elements and onClicks from options.fxml to OptionsController.java

--- src/main/java/game/OptionsController.java
+++ src/main/java/game/OptionsController.java
@@ -1,5 +1,65 @@
 package game;
 
-public class OptionsController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OptionsController implements Initializable {
+
+    @FXML
+    private Button convertButton;
+
+    @FXML
+    private ChoiceBox<?> currencyBoxExchange;
+
+    @FXML
+    private ChoiceBox<?> currencyBoxUser;
+
+    @FXML
+    private ChoiceBox<?> themes;
+
+    @FXML
+    void applySettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void convertCurrency(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteData(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openAboutBox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openGitHubPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setNewSettings(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
