8bf72d3552bd6d101a7656d358dff657677a15dd
Jrt345 <JonTech543@protonmail.com>
2022-01-06 06:02:19 -0800

    Add Maven Shade plugin and also create MANIFEST.MF

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,13 @@ module com.company.gamblescramble {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens game to javafx.fxml;
     exports game;
     opens game.utils to javafx.fxml;
     exports game.utils;
     opens game.games to javafx.fxml;
     exports game.games;
-    exports game.controllers;
     opens game.controllers to javafx.fxml;
+    exports game.controllers;
+
 }
