56deeeccd2c900e80586ef1b5b3202f060499282
Jrt345 <JonTech543@protonmail.com>
2022-07-24 16:39:13 -0700

    Add variable and getters/setters to Ticket.java

--- src/main/java/game/games/scratchoff/Ticket.java
+++ src/main/java/game/games/scratchoff/Ticket.java
@@ -2,4 +2,31 @@ package game.games.scratchoff;
 
 public class Ticket {
 
+    private int multiplier = 1;
+    private int[] luckyNumbers = new int[3];
+    private int[][] gridNumbers = new int[3][3];
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public int[] getLuckyNumbers() {
+        return luckyNumbers;
+    }
+
+    public void setLuckyNumbers(int[] luckyNumbers) {
+        this.luckyNumbers = luckyNumbers;
+    }
+
+    public int[][] getGridNumbers() {
+        return gridNumbers;
+    }
+
+    public void setGridNumbers(int[][] gridNumbers) {
+        this.gridNumbers = gridNumbers;
+    }
 }
